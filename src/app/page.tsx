ae0a8b6acf9ea71afb3d8608520062b2e9d27b5d
Edmilson Soares <esbnet@gmail.com>
2023-05-16 14:54:10 -0300

    feat: :sparkles: setup next taiwind eslint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-
 export default function Home() {
   return (
-    <h1>Hello worf!</h1>
+    <div className="h-screen bg-zinc-950 p-6 text-zinc-50">
+      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
+    </div>
   )
 }
