b3377fd8195741454f86cc2949d264997abc0fc7
Edmilson Soares <esbnet@gmail.com>
2023-05-21 00:17:26 -0300

    feat: :sparkles: create page memory list

--- src/app/memories/new/page.tsx
+++ src/app/memories/new/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 export default function New() {
   return (
-    <div className="flex flex-1 flex-col gap-4">
+    <div className="flex flex-1 flex-col gap-4 p-8">
       <Link
         href="/"
         className="flex items-center gap-1 text-sm text-gray-200 hover:text-gray-100"
